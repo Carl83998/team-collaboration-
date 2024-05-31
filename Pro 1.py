c2869fd9ecad9e6b3cbd0c5a355fd04213b31b67
ProDesmond <nanafada28@gmail.com>
2024-05-30 18:12:05 -0700

    Project

---
+++ Pro 1.py
@@ -0,0 +1,14 @@
+# num = int(input("enter your first number : "))
+# num2 = int(input("enter your second number : "))
+# operator = input("enter any operator(+, -, * , /) : ")
+# if operator == "+" :
+#     print(num+num2)
+# elif operator == "-" :
+#     print(num - num2)
+# elif operator == "*" :
+#     print(num * num2)
+# elif operator == "/" :
+#     print(num / num2)
+# else :
+#     print("error")
+
